9929f80a30755aa1b84ceef43ece2871fc09cdd6
MinhNPH <hieunt9@example.com>
2021-03-27 17:01:58 +0700

    edit jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Adding apt-keys"
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 
 echo "Updating apt-get"
@@ -21,3 +21,5 @@ sudo apt-get -y install jenkins > /dev/null 2>&1
 sudo service jenkins start
 
 sleep 1m
+
+# ref commnad tee: https://www.geeksforgeeks.org/tee-command-linux-example/#:~:text=tee%20command%20reads%20the%20standard,and%20saved%20in%20a%20file.
